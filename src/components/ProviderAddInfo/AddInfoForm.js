2a434303e11f253e2f29f965f433f52e723a3a36
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-03-25 18:52:56 -0700

    new Schema and new AddInfoForm

--- src/components/ProviderAddInfo/AddInfoForm.js
+++ src/components/ProviderAddInfo/AddInfoForm.js
@@ -1,134 +1,137 @@
-import React, {Component} from 'react';
-import { withRouter } from 'react-router-dom';
-import {compose} from 'recompose';
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom";
+import {compose} from "recompose";
 
-import {withFirebase} from '../../server/Firebase/index';
-import {withAuthentication} from '../../server/Session/index';
+import {withFirebase} from "../../server/Firebase/index";
+import {withAuthentication} from "../../server/Session/index";
 
-import * as SCHEMA from '../../constants/schema';
-import * as ROUTES from '../../constants/routes';
-
+import * as SCHEMA from "../../constants/schema";
+import * as ROUTES from "../../constants/routes";
 
 const INITIAL_STATE = {
-  error: null,
-  isLoading: false,
+	error: null,
+	isLoading: false
+};
+
+class AddInfoFormBase extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			authUser: {
+				...SCHEMA.user
+			},
+			...INITIAL_STATE
+		};
+	}
+
+	onSubmit = event => {
+		this.props.firebase
+			.doUpdateProfile({
+				...this.state.authUser
+			})
+			.then(() => {
+				alert("Update Success");
+			})
+			.catch(error => {
+				alert(error);
+			});
+		event.preventDefault();
+	};
+
+	componentWillMount() {
+		this.setState({
+			authUser: {
+				displayName: {value: this.props.authUser.displayName},
+				email: {value: this.props.authUser.email},
+				phoneNumber: {value: this.props.authUser.phoneNumber},
+				gender: {value: this.props.authUser.gender},
+				photoURL: {value: this.props.authUser.photoURL}
+			}
+		});
+	}
+	onChange = propertyName => event => {
+		const {authUser} = this.state;
+		const newAuthUser = {
+			...authUser,
+			[propertyName]: event.target.value
+		};
+		this.setState({authUser: newAuthUser});
+	};
+	/**
+	 * check if the Schema Input is inValid
+	 * @param  {}  object [this.state.?checkObject?]
+	 * @return {Boolean}        [return if one is not satisfied]
+	 */
+	isInvalid = object =>
+		object.some(f => {
+			if (typeof f[1].value == "undefined")
+				throw new Error(`Schema ${f[0]} do not have value`);
+			else if (f[1].type === "text") {
+				console.log(f[1].type, f[0]);
+				return f[1].value === "";
+			} else {
+				throw new Error(`Did not match all type`);
+			}
+		});
+
+	render() {
+		const {error} = this.state;
+
+		const isInvalid = this.isInvalid(Object.entries(this.state.authUser));
+
+		return (
+			<form onSubmit={this.onSubmit}>
+				<input
+					value={this.state.authUser.displayName}
+					onChange={this.onChange("displayName")}
+					type="text"
+					placeholder="Full Name"
+				/>
+				<input
+					onChange={this.onChange("email")}
+					value={this.state.authUser.email}
+					type="text"
+					placeholder="email"
+				/>
+				<input
+					onChange={this.onChange("gender")}
+					type="text"
+					value={this.state.authUser.gender}
+					placeholder="gender"
+				/>
+				<input
+					onChange={this.onChange("phoneNumber")}
+					value={this.state.authUser.phoneNumber}
+					type="text"
+					placeholder="phoneNumber"
+				/>
+				{
+					//TODO: Add component to choose Agent or Client
+				}
+				{
+					//TODO: Add Real Avatar input
+				}
+				<input
+					onChange={this.onChange("photoURL")}
+					value={this.state.authUser.photoURL}
+					type="text"
+					placeholder="photoURL"
+				/>
+
+				<button disabled={isInvalid} type="submit">
+					Add new Information
+				</button>
+
+				{error && <p>{error.message}</p>}
+			</form>
+		);
+	}
 }
 
+const AddInfoForm = compose(
+	withAuthentication,
+	withFirebase
+)(AddInfoFormBase);
 
-class AddInfoFormBase extends Component{
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authUser: {
-        ...SCHEMA.user,
-      },
-      ...INITIAL_STATE,
-    }
-
-  }
-
-  onSubmit = event => {
-
-    this.props.firebase.doUpdateProfile({
-      ...this.state.authUser,
-    })
-    .then(() => {
-      alert("Update Success");
-    })
-    .catch(error =>{
-      alert(error);
-    })
-    event.preventDefault();
-  }
-
-  componentWillMount() {
-    this.setState({
-
-        authUser: {
-      displayName: this.props.authUser.displayName ,
-      email: this.props.authUser.email ,
-      phoneNumber: this.props.authUser.phoneNumber ,
-      gender: this.props.authUser.gender ,
-      photoURL: this.props.authUser.photoURL,
-    }
-    });
-
-  }
-  onChange = (propertyName) => (event) => {
-    const {authUser} = this.state;
-    const newAuthUser = {
-      ...authUser,
-      [propertyName]: event.target.value
-    }
-    this.setState({authUser: newAuthUser});
-  }
-
-  render() {
-    const {
-      error,
-    } = this.state;
-
-
-    const isInvalid =
-    this.state.authUser.displayName === '' ||
-    this.state.authUser.email === '' ||
-    this.state.authUser.gender === '' ||
-    this.state.authUser.phoneNumber === '';
-
-
-    return (
-      <form onSubmit={this.onSubmit}>
-      <input
-      value={this.state.authUser.displayName}
-      onChange={this.onChange('displayName')}
-      type="text"
-      placeholder="Full Name"
-      />
-      <input
-      onChange={this.onChange('email')}
-      value={this.state.authUser.email}
-      type="text"
-      placeholder="email"
-      />
-      <input
-      onChange={this.onChange('gender')}
-      type="text"
-      value={this.state.authUser.gender}
-      placeholder="gender"
-      />
-      <input
-      onChange={this.onChange('phoneNumber')}
-      value={this.state.authUser.phoneNumber}
-      type="text"
-      placeholder="phoneNumber"
-      />
-      {
-        //TODO: Add component to choose Agent or Client
-      }
-      {
-        //TODO: Add Real Avatar input
-      }
-      <input
-      onChange={this.onChange('photoURL')}
-      value={this.state.authUser.photoURL}
-      type="text"
-      placeholder="photoURL"
-      />
-
-      <button disabled={isInvalid} type="submit">
-      Add new Information
-      </button>
-
-      {error && <p>{error.message}</p>}
-      </form>
-    )}
-  }
-
-  const AddInfoForm = compose(
-    withAuthentication,
-    withFirebase,
-  )(AddInfoFormBase)
-
-  export default AddInfoForm;
+export default AddInfoForm;
--- src/constants/schema.js
+++ src/constants/schema.js
@@ -1,28 +1,109 @@
 const user = {
-  displayName: '',
-  email: '',
-  gender: '',
-  phoneNumber: '',
-  isAgent: false,
-  roles: [],
-  photoURL: '',
-}
+	displayName: {
+		loopable: true,
+		type: "text",
+		placeHolder: "User Name",
+		value: ""
+	},
+	email: {
+		loopable: true,
+		type: "text",
+		placeHolder: "Email",
+		value: ""
+	},
+	gender: {
+		loopable: true,
+		type: "text",
+		placeHolder: "Gender",
+		value: ""
+	},
+	phoneNumber: {
+		loopable: true,
+		type: "text",
+		placeHolder: "Phone Number",
+		value: ""
+	},
+	roles: {
+		loopable: false,
+		value: []
+	},
+	photoURL: {
+		loopable: false,
+		type: "img",
+		placeHolder: "Photo Avatar",
+		value: ""
+	}
+};
 
 const house = {
-  address: '',
-  price: '',
-  description: '',
-  data: '',
-  features: {
-    type: '',
-    yearBuild: '',
-    heating: '',
-    cooling: '',
-    parking: '',
-    lot: '',
-  },
-  agentId: [],
-  userID: [],
-}
+	address: {
+		street: {
+			loopable: true,
+			type: "text",
+			placeHolder: "House Address and Street Name",
+			value: ""
+		},
+		city: {
+			loopable: true,
+			type: "text",
+			placeHolder: "City",
+			value: ""
+		},
+		state: {
+			loopable: true,
+			type: "text",
+			placeHolder: "State",
+			value: ""
+		},
+		country: {
+			loopable: true,
+			type: "text",
+			placeHolder: "Country",
+			value: ""
+		},
+		location: {
+			loopable: false,
+			value: {
+				lat: 0.0,
+				long: 0.0
+			}
+		}
+	},
+	price: {
+		loopable: true,
+		type: "text",
+		placeHolder: "Price",
+		value: ""
+	},
+	description: {
+		loopable: true,
+		type: "text",
+		placeHolder: "Description",
+		value: ""
+	},
+	data: {
+		loopable: false,
+		value: ""
+	},
+	features: {
+		loopable: false,
+		value: {
+			type: "",
+			yearBuild: "",
+			heating: "",
+			cooling: "",
+			parking: "",
+			lot: ""
+		}
+	},
+	agentId: {
+		loopable: false,
+		value: []
+	},
+	userID: {
+		loopable: false,
+		value: []
+	}
+};
 
-export {user, house,}
+export {user, house};
