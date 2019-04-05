e9754b8e297d81f307ae82bbf68522615b9847b0
Hoa Nguyen <nguyenhoa2896@gmail.com>
2019-04-05 08:11:07 -0700

    update schema

--- src/constants/schema.js
+++ src/constants/schema.js
@@ -1,109 +1,40 @@
 const user = {
-	displayName: {
-		loopable: true,
-		type: "text",
-		placeHolder: "User Name",
-		value: ""
-	},
-	email: {
-		loopable: true,
-		type: "text",
-		placeHolder: "Email",
-		value: ""
-	},
-	gender: {
-		loopable: true,
-		type: "text",
-		placeHolder: "Gender",
-		value: ""
-	},
-	phoneNumber: {
-		loopable: true,
-		type: "text",
-		placeHolder: "Phone Number",
-		value: ""
-	},
-	roles: {
-		loopable: false,
-		value: []
-	},
-	photoURL: {
-		loopable: false,
-		type: "img",
-		placeHolder: "Photo Avatar",
-		value: ""
-	}
+	displayName: "",
+	email: "",
+	gender: "",
+	phoneNumber: "",
+	roles: [],
+	photoURL: ""
 };
 
 const house = {
 	address: {
-		street: {
-			loopable: true,
-			type: "text",
-			placeHolder: "House Address and Street Name",
-			value: ""
-		},
-		city: {
-			loopable: true,
-			type: "text",
-			placeHolder: "City",
-			value: ""
-		},
-		state: {
-			loopable: true,
-			type: "text",
-			placeHolder: "State",
-			value: ""
-		},
-		country: {
-			loopable: true,
-			type: "text",
-			placeHolder: "Country",
-			value: ""
-		},
-		location: {
-			loopable: false,
-			value: {
-				lat: 0.0,
-				long: 0.0
-			}
-		}
+		street: "",
+		city: "",
+		state: "",
+		country: "",
+		location: ""
 	},
-	price: {
-		loopable: true,
-		type: "text",
-		placeHolder: "Price",
-		value: ""
-	},
-	description: {
-		loopable: true,
-		type: "text",
-		placeHolder: "Description",
-		value: ""
-	},
-	data: {
-		loopable: false,
-		value: ""
-	},
+	price: "",
+	description: "",
+	data: "",
 	features: {
-		loopable: false,
-		value: {
-			type: "",
-			yearBuild: "",
-			heating: "",
-			cooling: "",
-			parking: "",
-			lot: ""
-		}
+		type: "",
+		yearBuild: "",
+		heating: "",
+		cooling: "",
+		parking: "",
+		lot: ""
 	},
-	agentId: {
-		loopable: false,
-		value: []
-	},
-	userID: {
-		loopable: false,
-		value: []
-	}
+	agentId: "",
+	userID: [],
+};
+const location = {
+	state: "",
+	city: "",
+	country: "",
+	zipcode: "",
+	hotels: ""
 };
 
-export {user, house};
+export {user, house, location};
